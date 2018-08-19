b4febdf92c187db02d92788fbd27e2e67563dd7a
Ferre Mekelenkamp <hallo@ferremekelenkamp.com>
2018-08-19 13:45:42 +0800

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>This domain is for sale!</h1>
+    <p>
+      Are you interested to buy the domain <strong>fuchsiaschool.com</strong>?<br />
+      Send an email to{' '}
+      <a href="mailto:hallo@ferremekelenkamp.com">hallo@ferremekelenkamp.com</a>.
+    </p>
+  </div>
+)
+
+export default IndexPage
